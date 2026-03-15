d8b43815cdb4a05e6b5c28bcc4c63bd00beb6094
Marc-Andre [macOS] <marcandre.voyer@gmail.com>
2026-03-14 20:58:25 -0400

    [Keymaps] Transform Tool Keymaps for E transform.rotate working in all modes. Displays fine in preferences UI too.

--- operators_handling/actions/keymaps/transform/transform_tools.py
+++ operators_handling/actions/keymaps/transform/transform_tools.py
@@ -35,101 +35,115 @@ def _translate_bindings(keymap_name: str, space_type: str = 'EMPTY', region_type
     )
 
 
+def _rotate_bindings(keymap_name: str, space_type: str = 'EMPTY', region_type: str = 'WINDOW') -> tuple[KeymapBinding, ...]:
+    """
+    Return the default Blue Hole keymap bindings for a 'rotate' style action.
+    """
+    return (
+        KeymapBinding(
+            keymap_name=keymap_name,
+            space_type=space_type,
+            region_type=region_type,
+            key='E',
+        ),
+    )
+
+
 # ----------------------------------------------------------------------------------------------------------------------
-# FEATURE: TRANSFORM TOOLS
+# FEATURE: TRANSFORM TOOLS (TRANSLATE)
 
 # Paint Curve
-TRANSFORM_PAINT_CURVE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_PAINT_CURVE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Paint Curve')
 )
 
 # Pose
-TRANSFORM_POSE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_POSE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Pose')
 )
 
 # Object Mode
-TRANSFORM_OBJECT_MODE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_OBJECT_MODE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Object Mode')
 )
 
 # Curve
-TRANSFORM_CURVE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_CURVE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Curve')
 )
 
 # Curves
-TRANSFORM_CURVES_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_CURVES = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Curves')
 )
 
 # Mesh
-TRANSFORM_MESH_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_MESH = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Mesh')
 )
 
 # Armature
-TRANSFORM_ARMATURE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_ARMATURE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Armature')
 )
 
 # Metaball
-TRANSFORM_METABALL_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_METABALL = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Metaball')
 )
 
 # Lattice
-TRANSFORM_LATTICE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_LATTICE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Lattice')
 )
 
 # Particle
-TRANSFORM_PARTICLE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_PARTICLE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Particle')
 )
 
 # Point Cloud
-TRANSFORM_POINT_CLOUD_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_POINT_CLOUD = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Point Cloud')
 )
 
 # Grease Pencil Edit Mode
-TRANSFORM_GREASE_PENCIL_EDIT_MODE_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_GREASE_PENCIL_EDIT_MODE = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Grease Pencil Edit Mode')
 )
 
 # UV Editor
-TRANSFORM_UV_EDITOR_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_UV_EDITOR = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('UV Editor')
 )
 
 # Mask Editing
-TRANSFORM_MASK_EDITING_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_MASK_EDITING = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Mask Editing')
 )
 
 # Graph Editor
-TRANSFORM_GRAPH_EDITOR_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_GRAPH_EDITOR = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Graph Editor', space_type='GRAPH_EDITOR')
 )
 
 # Node Editor
-TRANSFORM_NODE_EDITOR_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_NODE_EDITOR = OperatorAction(
     operator='transform.translate',
     props={
         'view2d_edge_pan': True,
@@ -138,21 +152,138 @@ TRANSFORM_NODE_EDITOR_TRANSLATE = OperatorAction(
 )
 
 # Preview
-TRANSFORM_PREVIEW_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_PREVIEW = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Preview', space_type='SEQUENCE_EDITOR')
 )
 
 # Clip Editor
-TRANSFORM_CLIP_EDITOR_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_CLIP_EDITOR = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Clip Editor', space_type='CLIP_EDITOR')
 )
 
 # Clip Graph Editor
-TRANSFORM_CLIP_GRAPH_EDITOR_TRANSLATE = OperatorAction(
+TRANSFORM_TRANSLATE_CLIP_GRAPH_EDITOR = OperatorAction(
     operator='transform.translate',
     keymap_bindings=_translate_bindings('Clip Graph Editor', space_type='CLIP_EDITOR')
+)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FEATURE: TRANSFORM TOOLS (ROTATE)
+
+# Paint Curve
+TRANSFORM_ROTATE_PAINT_CURVE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Paint Curve')
+)
+
+# Pose
+TRANSFORM_ROTATE_POSE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Pose')
+)
+
+# Object Mode
+TRANSFORM_ROTATE_OBJECT_MODE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Object Mode')
+)
+
+# Curve
+TRANSFORM_ROTATE_CURVE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Curve')
+)
+
+# Curves
+TRANSFORM_ROTATE_CURVES = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Curves')
+)
+
+# Mesh
+TRANSFORM_ROTATE_MESH = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Mesh')
+)
+
+# Armature
+TRANSFORM_ROTATE_ARMATURE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Armature')
+)
+
+# Metaball
+TRANSFORM_ROTATE_METABALL = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Metaball')
+)
+
+# Lattice
+TRANSFORM_ROTATE_LATTICE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Lattice')
+)
+
+# Particle
+TRANSFORM_ROTATE_PARTICLE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Particle')
+)
+
+# Point Cloud
+TRANSFORM_ROTATE_POINT_CLOUD = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Point Cloud')
+)
+
+# Grease Pencil Edit Mode
+TRANSFORM_ROTATE_GREASE_PENCIL_EDIT_MODE = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Grease Pencil Edit Mode')
+)
+
+# UV Editor
+TRANSFORM_ROTATE_UV_EDITOR = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('UV Editor')
+)
+
+# Mask Editing
+TRANSFORM_ROTATE_MASK_EDITING = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Mask Editing')
+)
+
+# Graph Editor
+TRANSFORM_ROTATE_GRAPH_EDITOR = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Graph Editor', space_type='GRAPH_EDITOR')
+)
+
+# Node Editor
+TRANSFORM_ROTATE_NODE_EDITOR = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Node Editor', space_type='NODE_EDITOR')
+)
+
+# Preview
+TRANSFORM_ROTATE_PREVIEW = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Preview', space_type='SEQUENCE_EDITOR')
+)
+
+# Clip Editor
+TRANSFORM_ROTATE_CLIP_EDITOR = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Clip Editor', space_type='CLIP_EDITOR')
+)
+
+# Clip Graph Editor
+TRANSFORM_ROTATE_CLIP_GRAPH_EDITOR = OperatorAction(
+    operator='transform.rotate',
+    keymap_bindings=_rotate_bindings('Clip Graph Editor', space_type='CLIP_EDITOR')
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -161,27 +292,46 @@ TRANSFORM_CLIP_GRAPH_EDITOR_TRANSLATE = OperatorAction(
 TRANSFORM_TOOLS_ACTIONS: list[OperatorAction] = [
 
     # Translate
-    TRANSFORM_PAINT_CURVE_TRANSLATE,
-    TRANSFORM_POSE_TRANSLATE,
-    TRANSFORM_OBJECT_MODE_TRANSLATE,
-    TRANSFORM_CURVE_TRANSLATE,
-    TRANSFORM_CURVES_TRANSLATE,
-    TRANSFORM_MESH_TRANSLATE,
-    TRANSFORM_ARMATURE_TRANSLATE,
-    TRANSFORM_METABALL_TRANSLATE,
-    TRANSFORM_LATTICE_TRANSLATE,
-    TRANSFORM_PARTICLE_TRANSLATE,
-    TRANSFORM_POINT_CLOUD_TRANSLATE,
-    TRANSFORM_GREASE_PENCIL_EDIT_MODE_TRANSLATE,
-    TRANSFORM_UV_EDITOR_TRANSLATE,
-    TRANSFORM_MASK_EDITING_TRANSLATE,
-    TRANSFORM_GRAPH_EDITOR_TRANSLATE,
-    TRANSFORM_NODE_EDITOR_TRANSLATE,
-    TRANSFORM_PREVIEW_TRANSLATE,
-    TRANSFORM_CLIP_EDITOR_TRANSLATE,
-    TRANSFORM_CLIP_GRAPH_EDITOR_TRANSLATE,
+    TRANSFORM_TRANSLATE_PAINT_CURVE,
+    TRANSFORM_TRANSLATE_POSE,
+    TRANSFORM_TRANSLATE_OBJECT_MODE,
+    TRANSFORM_TRANSLATE_CURVE,
+    TRANSFORM_TRANSLATE_CURVES,
+    TRANSFORM_TRANSLATE_MESH,
+    TRANSFORM_TRANSLATE_ARMATURE,
+    TRANSFORM_TRANSLATE_METABALL,
+    TRANSFORM_TRANSLATE_LATTICE,
+    TRANSFORM_TRANSLATE_PARTICLE,
+    TRANSFORM_TRANSLATE_POINT_CLOUD,
+    TRANSFORM_TRANSLATE_GREASE_PENCIL_EDIT_MODE,
+    TRANSFORM_TRANSLATE_UV_EDITOR,
+    TRANSFORM_TRANSLATE_MASK_EDITING,
+    TRANSFORM_TRANSLATE_GRAPH_EDITOR,
+    TRANSFORM_TRANSLATE_NODE_EDITOR,
+    TRANSFORM_TRANSLATE_PREVIEW,
+    TRANSFORM_TRANSLATE_CLIP_EDITOR,
+    TRANSFORM_TRANSLATE_CLIP_GRAPH_EDITOR,
 
     # Rotate
+    TRANSFORM_ROTATE_PAINT_CURVE,
+    TRANSFORM_ROTATE_POSE,
+    TRANSFORM_ROTATE_OBJECT_MODE,
+    TRANSFORM_ROTATE_CURVE,
+    TRANSFORM_ROTATE_CURVES,
+    TRANSFORM_ROTATE_MESH,
+    TRANSFORM_ROTATE_ARMATURE,
+    TRANSFORM_ROTATE_METABALL,
+    TRANSFORM_ROTATE_LATTICE,
+    TRANSFORM_ROTATE_PARTICLE,
+    TRANSFORM_ROTATE_POINT_CLOUD,
+    TRANSFORM_ROTATE_GREASE_PENCIL_EDIT_MODE,
+    TRANSFORM_ROTATE_UV_EDITOR,
+    TRANSFORM_ROTATE_MASK_EDITING,
+    TRANSFORM_ROTATE_GRAPH_EDITOR,
+    TRANSFORM_ROTATE_NODE_EDITOR,
+    TRANSFORM_ROTATE_PREVIEW,
+    TRANSFORM_ROTATE_CLIP_EDITOR,
+    TRANSFORM_ROTATE_CLIP_GRAPH_EDITOR,
 
     # Scale
 
