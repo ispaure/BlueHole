8d2398bc4baf8b77400d29e8741e4dbc0043fec2
Marc-Andre [macOS] <marcandre.voyer@gmail.com>
2026-03-14 21:05:56 -0400

    [Keymaps] Transform Tool Keymaps for R transform.resize working in all modes. Displays fine in preferences UI too

--- operators_handling/actions/keymaps/transform/transform_tools.py
+++ operators_handling/actions/keymaps/transform/transform_tools.py
@@ -49,6 +49,20 @@ def _rotate_bindings(keymap_name: str, space_type: str = 'EMPTY', region_type: s
     )
 
 
+def _resize_bindings(keymap_name: str, space_type: str = 'EMPTY', region_type: str = 'WINDOW') -> tuple[KeymapBinding, ...]:
+    """
+    Return the default Blue Hole keymap bindings for a 'resize' style action.
+    """
+    return (
+        KeymapBinding(
+            keymap_name=keymap_name,
+            space_type=space_type,
+            region_type=region_type,
+            key='R',
+        ),
+    )
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 # FEATURE: TRANSFORM TOOLS (TRANSLATE)
 
@@ -287,6 +301,123 @@ TRANSFORM_ROTATE_CLIP_GRAPH_EDITOR = OperatorAction(
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
+# FEATURE: TRANSFORM TOOLS (RESIZE)
+
+# Paint Curve
+TRANSFORM_RESIZE_PAINT_CURVE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Paint Curve')
+)
+
+# Pose
+TRANSFORM_RESIZE_POSE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Pose')
+)
+
+# Object Mode
+TRANSFORM_RESIZE_OBJECT_MODE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Object Mode')
+)
+
+# Curve
+TRANSFORM_RESIZE_CURVE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Curve')
+)
+
+# Curves
+TRANSFORM_RESIZE_CURVES = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Curves')
+)
+
+# Mesh
+TRANSFORM_RESIZE_MESH = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Mesh')
+)
+
+# Armature
+TRANSFORM_RESIZE_ARMATURE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Armature')
+)
+
+# Metaball
+TRANSFORM_RESIZE_METABALL = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Metaball')
+)
+
+# Lattice
+TRANSFORM_RESIZE_LATTICE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Lattice')
+)
+
+# Particle
+TRANSFORM_RESIZE_PARTICLE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Particle')
+)
+
+# Point Cloud
+TRANSFORM_RESIZE_POINT_CLOUD = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Point Cloud')
+)
+
+# Grease Pencil Edit Mode
+TRANSFORM_RESIZE_GREASE_PENCIL_EDIT_MODE = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Grease Pencil Edit Mode')
+)
+
+# UV Editor
+TRANSFORM_RESIZE_UV_EDITOR = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('UV Editor')
+)
+
+# Mask Editing
+TRANSFORM_RESIZE_MASK_EDITING = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Mask Editing')
+)
+
+# Graph Editor
+TRANSFORM_RESIZE_GRAPH_EDITOR = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Graph Editor', space_type='GRAPH_EDITOR')
+)
+
+# Node Editor
+TRANSFORM_RESIZE_NODE_EDITOR = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Node Editor', space_type='NODE_EDITOR')
+)
+
+# Preview
+TRANSFORM_RESIZE_PREVIEW = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Preview', space_type='SEQUENCE_EDITOR')
+)
+
+# Clip Editor
+TRANSFORM_RESIZE_CLIP_EDITOR = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Clip Editor', space_type='CLIP_EDITOR')
+)
+
+# Clip Graph Editor
+TRANSFORM_RESIZE_CLIP_GRAPH_EDITOR = OperatorAction(
+    operator='transform.resize',
+    keymap_bindings=_resize_bindings('Clip Graph Editor', space_type='CLIP_EDITOR')
+)
+
+# ----------------------------------------------------------------------------------------------------------------------
 # ACTION LIST
 
 TRANSFORM_TOOLS_ACTIONS: list[OperatorAction] = [
@@ -334,6 +465,25 @@ TRANSFORM_TOOLS_ACTIONS: list[OperatorAction] = [
     TRANSFORM_ROTATE_CLIP_GRAPH_EDITOR,
 
     # Scale
+    TRANSFORM_RESIZE_PAINT_CURVE,
+    TRANSFORM_RESIZE_POSE,
+    TRANSFORM_RESIZE_OBJECT_MODE,
+    TRANSFORM_RESIZE_CURVE,
+    TRANSFORM_RESIZE_CURVES,
+    TRANSFORM_RESIZE_MESH,
+    TRANSFORM_RESIZE_ARMATURE,
+    TRANSFORM_RESIZE_METABALL,
+    TRANSFORM_RESIZE_LATTICE,
+    TRANSFORM_RESIZE_PARTICLE,
+    TRANSFORM_RESIZE_POINT_CLOUD,
+    TRANSFORM_RESIZE_GREASE_PENCIL_EDIT_MODE,
+    TRANSFORM_RESIZE_UV_EDITOR,
+    TRANSFORM_RESIZE_MASK_EDITING,
+    TRANSFORM_RESIZE_GRAPH_EDITOR,
+    TRANSFORM_RESIZE_NODE_EDITOR,
+    TRANSFORM_RESIZE_PREVIEW,
+    TRANSFORM_RESIZE_CLIP_EDITOR,
+    TRANSFORM_RESIZE_CLIP_GRAPH_EDITOR,
 
 ]
 
