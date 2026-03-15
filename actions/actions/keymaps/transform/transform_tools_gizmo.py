833811f3bce35edfdb60dcb302183b8c99e8d608
Marc-Andre [macOS] <marcandre.voyer@gmail.com>
2026-03-15 16:02:09 -0400

    [UI] [Transform Tools Gizmo & Modal] Improved names in the Blue Hole keymaps UI so it is now more descriptive

--- actions/actions/keymaps/transform/transform_tools_gizmo.py
+++ actions/actions/keymaps/transform/transform_tools_gizmo.py
@@ -71,60 +71,70 @@ def _builtin_scale_bindings(keymap_name: str, space_type: str = 'VIEW_3D', regio
 # MOVE
 TRANSFORM_TOOL_POSE_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Pose', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_OBJECT_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Object Mode', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_CURVE_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Curve', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_CURVES_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Curves', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_MESH_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Mesh', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_ARMATURE_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Armature', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_METABALL_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Metaball', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_LATTICE_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Lattice', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_UV_EDITOR_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('UV Editor', space_type='IMAGE_EDITOR')
 )
 
 TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_MOVE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Move Tool (Gizmo)',
     props={'name': 'builtin.move'},
     keymap_bindings=_builtin_move_bindings('Grease Pencil Edit Mode', space_type='VIEW_3D')
 )
@@ -132,60 +142,70 @@ TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_MOVE = OperatorAction(
 # ROTATE
 TRANSFORM_TOOL_POSE_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Pose', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_OBJECT_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Object Mode', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_CURVE_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Curve', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_CURVES_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Curves', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_MESH_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Mesh', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_ARMATURE_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Armature', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_METABALL_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Metaball', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_LATTICE_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Lattice', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_UV_EDITOR_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('UV Editor', space_type='IMAGE_EDITOR')
 )
 
 TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_ROTATE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Rotate Tool (Gizmo)',
     props={'name': 'builtin.rotate'},
     keymap_bindings=_builtin_rotate_bindings('Grease Pencil Edit Mode', space_type='VIEW_3D')
 )
@@ -193,60 +213,70 @@ TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_ROTATE = OperatorAction(
 # SCALE
 TRANSFORM_TOOL_POSE_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Pose', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_OBJECT_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Object Mode', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_CURVE_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Curve', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_CURVES_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Curves', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_MESH_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Mesh', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_ARMATURE_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Armature', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_METABALL_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Metaball', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_LATTICE_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Lattice', space_type='VIEW_3D')
 )
 
 TRANSFORM_TOOL_UV_EDITOR_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('UV Editor', space_type='IMAGE_EDITOR')
 )
 
 TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_SCALE = OperatorAction(
     operator='wm.tool_set_by_id',
+    text='Scale Tool (Gizmo)',
     props={'name': 'builtin.scale'},
     keymap_bindings=_builtin_scale_bindings('Grease Pencil Edit Mode', space_type='VIEW_3D')
 )
@@ -255,7 +285,6 @@ TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_SCALE = OperatorAction(
 # ACTION LIST
 
 TRANSFORM_TOOLS_GIZMO_ACTIONS: list[OperatorAction] = [
-    # Move
     TRANSFORM_TOOL_POSE_MOVE,
     TRANSFORM_TOOL_OBJECT_MOVE,
     TRANSFORM_TOOL_CURVE_MOVE,
@@ -267,7 +296,6 @@ TRANSFORM_TOOLS_GIZMO_ACTIONS: list[OperatorAction] = [
     TRANSFORM_TOOL_UV_EDITOR_MOVE,
     TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_MOVE,
 
-    # Rotate
     TRANSFORM_TOOL_POSE_ROTATE,
     TRANSFORM_TOOL_OBJECT_ROTATE,
     TRANSFORM_TOOL_CURVE_ROTATE,
@@ -279,7 +307,6 @@ TRANSFORM_TOOLS_GIZMO_ACTIONS: list[OperatorAction] = [
     TRANSFORM_TOOL_UV_EDITOR_ROTATE,
     TRANSFORM_TOOL_GREASE_PENCIL_EDIT_MODE_ROTATE,
 
-    # Scale
     TRANSFORM_TOOL_POSE_SCALE,
     TRANSFORM_TOOL_OBJECT_SCALE,
     TRANSFORM_TOOL_CURVE_SCALE,
--- actions/actions/keymaps/transform/transform_tools_modal.py
+++ actions/actions/keymaps/transform/transform_tools_modal.py
@@ -65,354 +65,352 @@ def _resize_bindings(keymap_name: str, space_type: str = 'EMPTY', region_type: s
 # ----------------------------------------------------------------------------------------------------------------------
 # FEATURE: TRANSFORM MODAL (TRANSLATE)
 
-# Paint Curve
 TRANSFORM_TRANSLATE_PAINT_CURVE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Paint Curve')
 )
 
-# Pose
 TRANSFORM_TRANSLATE_POSE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Pose')
 )
 
-# Object Mode
 TRANSFORM_TRANSLATE_OBJECT_MODE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Object Mode')
 )
 
-# Curve
 TRANSFORM_TRANSLATE_CURVE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Curve')
 )
 
-# Curves
 TRANSFORM_TRANSLATE_CURVES = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Curves')
 )
 
-# Mesh
 TRANSFORM_TRANSLATE_MESH = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Mesh')
 )
 
-# Armature
 TRANSFORM_TRANSLATE_ARMATURE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Armature')
 )
 
-# Metaball
 TRANSFORM_TRANSLATE_METABALL = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Metaball')
 )
 
-# Lattice
 TRANSFORM_TRANSLATE_LATTICE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Lattice')
 )
 
-# Particle
 TRANSFORM_TRANSLATE_PARTICLE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Particle')
 )
 
-# Point Cloud
 TRANSFORM_TRANSLATE_POINT_CLOUD = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Point Cloud')
 )
 
-# Grease Pencil Edit Mode
 TRANSFORM_TRANSLATE_GREASE_PENCIL_EDIT_MODE = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Grease Pencil Edit Mode')
 )
 
-# UV Editor
 TRANSFORM_TRANSLATE_UV_EDITOR = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('UV Editor')
 )
 
-# Mask Editing
 TRANSFORM_TRANSLATE_MASK_EDITING = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Mask Editing')
 )
 
-# Graph Editor
 TRANSFORM_TRANSLATE_GRAPH_EDITOR = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Graph Editor', space_type='GRAPH_EDITOR')
 )
 
-# Node Editor
 TRANSFORM_TRANSLATE_NODE_EDITOR = OperatorAction(
     operator='transform.translate',
-    props={
-        'view2d_edge_pan': True,
-    },
+    text='Move Tool (Modal)',
+    props={'view2d_edge_pan': True},
     keymap_bindings=_translate_bindings('Node Editor', space_type='NODE_EDITOR')
 )
 
-# Preview
 TRANSFORM_TRANSLATE_PREVIEW = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Preview', space_type='SEQUENCE_EDITOR')
 )
 
-# Clip Editor
 TRANSFORM_TRANSLATE_CLIP_EDITOR = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Clip Editor', space_type='CLIP_EDITOR')
 )
 
-# Clip Graph Editor
 TRANSFORM_TRANSLATE_CLIP_GRAPH_EDITOR = OperatorAction(
     operator='transform.translate',
+    text='Move Tool (Modal)',
     keymap_bindings=_translate_bindings('Clip Graph Editor', space_type='CLIP_EDITOR')
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
 # FEATURE: TRANSFORM MODAL (ROTATE)
 
-# Paint Curve
 TRANSFORM_ROTATE_PAINT_CURVE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Paint Curve')
 )
 
-# Pose
 TRANSFORM_ROTATE_POSE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Pose')
 )
 
-# Object Mode
 TRANSFORM_ROTATE_OBJECT_MODE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Object Mode')
 )
 
-# Curve
 TRANSFORM_ROTATE_CURVE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Curve')
 )
 
-# Curves
 TRANSFORM_ROTATE_CURVES = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Curves')
 )
 
-# Mesh
 TRANSFORM_ROTATE_MESH = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Mesh')
 )
 
-# Armature
 TRANSFORM_ROTATE_ARMATURE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Armature')
 )
 
-# Metaball
 TRANSFORM_ROTATE_METABALL = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Metaball')
 )
 
-# Lattice
 TRANSFORM_ROTATE_LATTICE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Lattice')
 )
 
-# Particle
 TRANSFORM_ROTATE_PARTICLE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Particle')
 )
 
-# Point Cloud
 TRANSFORM_ROTATE_POINT_CLOUD = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Point Cloud')
 )
 
-# Grease Pencil Edit Mode
 TRANSFORM_ROTATE_GREASE_PENCIL_EDIT_MODE = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Grease Pencil Edit Mode')
 )
 
-# UV Editor
 TRANSFORM_ROTATE_UV_EDITOR = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('UV Editor')
 )
 
-# Mask Editing
 TRANSFORM_ROTATE_MASK_EDITING = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Mask Editing')
 )
 
-# Graph Editor
 TRANSFORM_ROTATE_GRAPH_EDITOR = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Graph Editor', space_type='GRAPH_EDITOR')
 )
 
-# Node Editor
 TRANSFORM_ROTATE_NODE_EDITOR = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Node Editor', space_type='NODE_EDITOR')
 )
 
-# Preview
 TRANSFORM_ROTATE_PREVIEW = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Preview', space_type='SEQUENCE_EDITOR')
 )
 
-# Clip Editor
 TRANSFORM_ROTATE_CLIP_EDITOR = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Clip Editor', space_type='CLIP_EDITOR')
 )
 
-# Clip Graph Editor
 TRANSFORM_ROTATE_CLIP_GRAPH_EDITOR = OperatorAction(
     operator='transform.rotate',
+    text='Rotate Tool (Modal)',
     keymap_bindings=_rotate_bindings('Clip Graph Editor', space_type='CLIP_EDITOR')
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
 # FEATURE: TRANSFORM MODAL (RESIZE)
 
-# Paint Curve
 TRANSFORM_RESIZE_PAINT_CURVE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Paint Curve')
 )
 
-# Pose
 TRANSFORM_RESIZE_POSE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Pose')
 )
 
-# Object Mode
 TRANSFORM_RESIZE_OBJECT_MODE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Object Mode')
 )
 
-# Curve
 TRANSFORM_RESIZE_CURVE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Curve')
 )
 
-# Curves
 TRANSFORM_RESIZE_CURVES = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Curves')
 )
 
-# Mesh
 TRANSFORM_RESIZE_MESH = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Mesh')
 )
 
-# Armature
 TRANSFORM_RESIZE_ARMATURE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Armature')
 )
 
-# Metaball
 TRANSFORM_RESIZE_METABALL = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Metaball')
 )
 
-# Lattice
 TRANSFORM_RESIZE_LATTICE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Lattice')
 )
 
-# Particle
 TRANSFORM_RESIZE_PARTICLE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Particle')
 )
 
-# Point Cloud
 TRANSFORM_RESIZE_POINT_CLOUD = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Point Cloud')
 )
 
-# Grease Pencil Edit Mode
 TRANSFORM_RESIZE_GREASE_PENCIL_EDIT_MODE = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Grease Pencil Edit Mode')
 )
 
-# UV Editor
 TRANSFORM_RESIZE_UV_EDITOR = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('UV Editor')
 )
 
-# Mask Editing
 TRANSFORM_RESIZE_MASK_EDITING = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Mask Editing')
 )
 
-# Graph Editor
 TRANSFORM_RESIZE_GRAPH_EDITOR = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Graph Editor', space_type='GRAPH_EDITOR')
 )
 
-# Node Editor
 TRANSFORM_RESIZE_NODE_EDITOR = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Node Editor', space_type='NODE_EDITOR')
 )
 
-# Preview
 TRANSFORM_RESIZE_PREVIEW = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Preview', space_type='SEQUENCE_EDITOR')
 )
 
-# Clip Editor
 TRANSFORM_RESIZE_CLIP_EDITOR = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Clip Editor', space_type='CLIP_EDITOR')
 )
 
-# Clip Graph Editor
 TRANSFORM_RESIZE_CLIP_GRAPH_EDITOR = OperatorAction(
     operator='transform.resize',
+    text='Scale Tool (Modal)',
     keymap_bindings=_resize_bindings('Clip Graph Editor', space_type='CLIP_EDITOR')
 )
 
@@ -420,7 +418,6 @@ TRANSFORM_RESIZE_CLIP_GRAPH_EDITOR = OperatorAction(
 # ACTION LIST
 
 TRANSFORM_TOOLS_MODAL_ACTIONS: list[OperatorAction] = [
-    # Translate
     TRANSFORM_TRANSLATE_PAINT_CURVE,
     TRANSFORM_TRANSLATE_POSE,
     TRANSFORM_TRANSLATE_OBJECT_MODE,
@@ -441,7 +438,6 @@ TRANSFORM_TOOLS_MODAL_ACTIONS: list[OperatorAction] = [
     TRANSFORM_TRANSLATE_CLIP_EDITOR,
     TRANSFORM_TRANSLATE_CLIP_GRAPH_EDITOR,
 
-    # Rotate
     TRANSFORM_ROTATE_PAINT_CURVE,
     TRANSFORM_ROTATE_POSE,
     TRANSFORM_ROTATE_OBJECT_MODE,
@@ -462,7 +458,6 @@ TRANSFORM_TOOLS_MODAL_ACTIONS: list[OperatorAction] = [
     TRANSFORM_ROTATE_CLIP_EDITOR,
     TRANSFORM_ROTATE_CLIP_GRAPH_EDITOR,
 
-    # Resize
     TRANSFORM_RESIZE_PAINT_CURVE,
     TRANSFORM_RESIZE_POSE,
     TRANSFORM_RESIZE_OBJECT_MODE,
