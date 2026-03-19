5dd28a97c13966512943b70d42a910b86eaa645c
marca[MBA] <marcandre.voyer@gmail.com>
2026-03-19 17:28:17 -0400

    [Hotfix] Fixed W/E/R not working (was stupid typo error of auto_constraint instead of auto_constraints

--- __init__.py
+++ __init__.py
@@ -43,7 +43,7 @@ bl_info = {
     "author": "Marc-André Voyer",
     "description": "",
     "blender": (4, 5, 1),
-    "version": (6, 3, 18),
+    "version": (6, 3, 19),
     "location": "",
     "warning": "",
     "category": "Generic",
