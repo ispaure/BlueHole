b2ea337df2d24806b8163da4c4b2ab6ba78d5991
Marc-Andre [macOS] <marcandre.voyer@gmail.com>
2026-05-13 17:13:30 -0400

    [Tech Art] Changing version info.

--- __init__.py
+++ __init__.py
@@ -47,7 +47,7 @@ bl_info = {
     "author": "Marc-André Voyer",
     "description": "",
     "blender": (4, 5, 1),
-    "version": (6, 3, 24),
+    "version": (6, 5, 13),
     "location": "",
     "warning": "",
     "category": "Generic",
